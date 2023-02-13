a8727e659a89a66d9d61ea2db0aa0c3cbb1a4dc9
Ilaedna Delmiro <115794672+ilaedna16@users.noreply.github.com>
2023-02-13 18:50:33 -0300

    Create  Fa-a-um-programa-para-calcular-o-volume-de-uma-caixa-retangular.-Para-isto-utilize-a-seguinte-f-rmu.c

---
+++ Fa-a-um-programa-para-calcular-o-volume-de-uma-caixa-retangular.-Para-isto-utilize-a-seguinte-f-rmu.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+  
+ int altura; 
+ int largura; 
+ int v; 
+ int c; 
+ 
+ 
+printf ("informe a altura\n"); 
+scanf ("%d",& altura);
+printf ("informe a largura\n"); 
+scanf ("%d",&largura); 
+
+v =c *largura *altura;
+
+printf ("os valores do comprimento da largura e da altura eh:%.d",v); 
+  
+    return 0;
+}
